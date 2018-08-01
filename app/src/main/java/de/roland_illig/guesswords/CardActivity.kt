599cf954fc85171e35e1ce27a69c1ae9b274c9a1
Roland Illig <roland.illig@gmx.de>
2018-08-02 01:39:23 +0200

    Migrate Handler back to Timer
    Having scheduleAtFixedRate is quite important. Using the handler made
    the "1 minute" round take about 100 seconds. Now only cheap operations
    are run often, while possibly expensive operations like loading and
    saving the state are only run once per second.

--- app/src/main/java/de/roland_illig/guesswords/CardActivity.kt
+++ app/src/main/java/de/roland_illig/guesswords/CardActivity.kt
@@ -7,12 +7,16 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
+import java.util.Timer
+import kotlin.concurrent.scheduleAtFixedRate
 
 class CardActivity : AppCompatActivity() {
 
     private lateinit var progress: ProgressBar
     private var buttonsEnabled = false
-    private var active = false
+    private lateinit var timer: Timer
+    private lateinit var handler: Handler
+    private lateinit var state: GameState
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,30 +25,31 @@ class CardActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        active = true
         progress = findViewById(R.id.progress)
         progress.max = withGameState(this) { it.totalMillis }
-        Handler().postDelayed(::tick, 100)
+        handler = Handler()
+        timer = Timer()
+        state = loadGameState(this)
+
+        timer.scheduleAtFixedRate(0, 100) { updateProgressBar() }
+        timer.scheduleAtFixedRate(1000, 1000) { state.save(this@CardActivity) }
+
         updateCard()
     }
 
     override fun onPause() {
-        active = false
         super.onPause()
+        timer.cancel()
     }
 
-    private fun tick() {
-        withGameState(this) { state ->
-            state.timePasses(100)
-            progress.progress = state.totalMillis - state.remainingMillis
-            if (state.remainingMillis == 0) {
-                state.nextCard()
-                state.nextTeam()
-                setResult(RESULT_OK, Intent().putExtra("enableButton", false))
-                finish()
-            } else if (active) {
-                Handler().postDelayed(::tick, 100)
-            }
+    private fun updateProgressBar() {
+        state.timePasses(100)
+        progress.progress = state.totalMillis - state.remainingMillis
+        if (state.remainingMillis == 0) {
+            state.nextCard()
+            state.nextTeam()
+            setResult(RESULT_OK, Intent().putExtra("enableButton", false))
+            finish()
         }
     }
 
