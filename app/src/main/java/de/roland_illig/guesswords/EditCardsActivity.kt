a6016a95ff2226ac7018e634e45e38ed1ef0d228
Roland Illig <roland.illig@gmx.de>
2018-10-14 00:52:45 +0200

    Add EditCardsActivity, currently not enabled in the main menu

---
+++ app/src/main/java/de/roland_illig/guesswords/EditCardsActivity.kt
@@ -0,0 +1,12 @@
+package de.roland_illig.guesswords
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EditCardsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_cards)
+    }
+}
